7f0e35c035f74f2bc679cde2e830bf9b1fb1cd8f
Mariia <coldrainLV@gmail.com>
2024-03-19 14:22:09 +0200

    Initial commit

---
+++ src/main/java/com/example/firestorewebapi/FirestoreWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.firestorewebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirestoreWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirestoreWebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/firestorewebapi/FirestoreWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firestorewebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirestoreWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
