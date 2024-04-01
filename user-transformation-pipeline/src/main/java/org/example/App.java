17bb8a16d34cc910c9fbcfea2befbcede1f6ddbd
yura <>
2024-04-01 15:53:09 +0300

    Adds pubsub listener

--- user-transformation-pipeline/src/main/java/org/example/App.java
+++ user-transformation-pipeline/src/main/java/org/example/App.java
@@ -1,12 +1,20 @@
 package org.example;
 
+import org.example.service.PubSubListener;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class App {
 
   public static void main(String[] args) {
     SpringApplication.run(App.class, args);
+  }
+
+  @Bean
+  CommandLineRunner commandLineRunner(PubSubListener listener) {
+    return args -> listener.listenToPubSub();
   }
 }
---
+++ user-transformation-pipeline/src/main/java/org/example/service/PubSubListener.java
@@ -0,0 +1,56 @@
+package org.example.service;
+
+import com.google.cloud.pubsub.v1.AckReplyConsumer;
+import com.google.cloud.pubsub.v1.MessageReceiver;
+import com.google.cloud.pubsub.v1.Subscriber;
+import com.google.pubsub.v1.PubsubMessage;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.google.pubsub.v1.ProjectSubscriptionName;
+
+@Service
+public class PubSubListener {
+
+  @Value("${cloud.gcp.projectId}")
+  private String projectId;
+
+  @Value("${cloud.gcp.services.pubSub.subscriptionId}")
+  private String subscriptionId;
+
+  public void listenToPubSub() {
+
+    subscribeAsyncExample(projectId, subscriptionId);
+  }
+
+  public static void subscribeAsyncExample(String projectId, String subscriptionId) {
+    ProjectSubscriptionName subscriptionName =
+        ProjectSubscriptionName.of(projectId, subscriptionId);
+
+    // Instantiate an asynchronous message receiver.
+    MessageReceiver receiver =
+        (PubsubMessage message, AckReplyConsumer consumer) -> {
+          // Handle incoming message, then ack the received message.
+          System.out.println("Id: " + message.getMessageId());
+          System.out.println("Data: " + message.getData().toStringUtf8());
+          consumer.ack();
+        };
+
+    Subscriber subscriber = null;
+    try {
+      subscriber = Subscriber.newBuilder(subscriptionName, receiver).build();
+      // Start the subscriber.
+      subscriber.startAsync().awaitRunning();
+      System.out.printf("Listening for messages on %s:\n", subscriptionName.toString());
+      // Allow the subscriber to run for 30s unless an unrecoverable error occurs.
+      subscriber.awaitTerminated(30, TimeUnit.SECONDS);
+    } catch (TimeoutException timeoutException) {
+      // Shut down the subscriber after 30s. Stop receiving messages.
+      subscriber.stopAsync();
+    }
+  }
+}
+
+
